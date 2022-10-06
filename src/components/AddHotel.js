ad4cc65a22fd8f012986be5bb48a208e1e921857
unknown <you@example.com>
2022-10-06 20:33:00 +0200

    admin

--- src/components/AddHotel.js
+++ src/components/AddHotel.js
@@ -39,9 +39,10 @@ const AddHotel = () => {
   const history = useHistory()
   const [_hotelName, setHotelName] = useState("")
   const [_location, setLocation] = useState("")
-  const [_days, setDays] = useState('')
-  const [_amount, setAmount] = useState("")
-  const [_totalAmount, setTotalAmount] = useState("")
+  const [days, setDays] = useState('')
+  const [amount, setAmount] = useState("")
+  const [_description, setDescription] = useState("")
+  const [totalAmount, setTotalAmount] = useState("")
   const [_availableRooms, setAvailableRooms] = useState("")
   const addHotelRef = collection(db, 'hotels')
   const [hotels,setHotels] = useState({})
@@ -65,7 +66,7 @@ const AddHotel = () => {
       "state_changed",
       (snapshot) => {
         const progressPercent = Math.round(
-          (snapshot.bytesTransfarred/snapshot.totalBytes)* 100
+          (snapshot.bytesTransferred/snapshot.totalBytes)* 100
         );
       },
       (err) => {
@@ -81,10 +82,11 @@ const AddHotel = () => {
           const hotel = {
             hotelName: _hotelName,
             location: _location,
-            days: _days,
-            amount: _amount,
+            days: days,
+            amount: amount,
+            description:_description,
             availableRooms: _availableRooms,
-            totalAmount: _days * _amount,
+            totalAmount: days * amount,
             image: url
           };
           addDoc(collectionRef,hotel).then(()=>{
@@ -146,9 +148,9 @@ const AddHotel = () => {
               <TextField id="outlined-basic" label="location" variant="standard" onChange={(e) => setLocation(e.target.value)} /><br></br>
               <TextField id="outlined-basic" label="days" variant="standard" onChange={(e) => setDays(e.target.value)} /><br></br>
               <TextField id="outlined-basic" label="Amount" variant="standard" onChange={(e) => setAmount(e.target.value)} /><br></br>
-              {/* <TextField id="outlined-basic" label="check in date" variant="standard" onChange={(e) => setCheckInDate(e.target.value)} /><br></br>
-              <TextField id="outlined-basic" label="check out date" variant="standard" onChange={(e) => setCheckOutDate(e.target.value)} /><br></br> */}
-              <TextField id="outlined-basic" label="Available room" variant="standard" onChange={(e) => setAvailableRooms(e.target.value)} /><br></br>
+              <TextField id="outlined-basic" label="Available Room" variant="standard" onChange={(e) => setAvailableRooms(e.target.value)} /><br></br>
+              
+              <TextField id="outlined-basic" label="description" variant="standard" onChange={(e) =>setDescription(e.target.value)} /><br></br>
               <TextField id="outlined-basic" label="Total Amount" style={{ display: 'none' }} variant="standard" onChange={(e) => setTotalAmount(e.target.value)} /><br></br>
               <input type='file' accept='image' onChange={(e)=>{handleImage(e)}}/>
               <Button onClick={(e) => { addHotel() }} className={classes.but} id="myButton" variant="contained" color="primary">
